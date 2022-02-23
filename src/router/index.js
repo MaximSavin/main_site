970643e7ec30845903e7ff1fd9e1caf2deb782e4
Maxim Savin <maxim.savin@gmail.com>
2022-02-23 13:04:54 +0700

    telegram isolated

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TelegramView from '../views/TelegramView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/telegram',
+      name: 'telegram',
+      component: TelegramView
     },
     // {
     //   path: '/about',
---
+++ src/views/TelegramView.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { computed, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import AuthModal from "@/components/AuthModal.vue";
+import TelegramLogin from "@/components/TLogin.vue";
+import RaceModal from "@/components/RaceModal.vue";
+import ruSvg from "@/assets/images/ru.svg";
+import enSvg from "@/assets/images/en.svg";
+
+const { t, locale, availableLocales } = useI18n({ useScope: "global" });
+const authModalShow = ref(false);
+const raceModalShow = ref(false);
+
+const toggleAuthModalShow = () => {
+  authModalShow.value = !authModalShow.value;
+};
+const toggleRaceModalShow = () => {
+  raceModalShow.value = !raceModalShow.value;
+};
+
+const toggleLang = () => {
+  const locales = availableLocales;
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length];
+};
+
+// console.log(new URL('../assets', import.meta.url).href)
+// console.log(new URL('../assets', import.meta.url))
+
+// const localeImgUrl = computed(() => new URL('../assets/images/', import.meta.url).href + locale.value + ".svg")
+const localeImgUrl = computed(() => (locale.value == "ru" ? ruSvg : enSvg));
+</script>
+
+<template>
+  <div class="uk-width-1-1"><TelegramLogin /></div>
+</template>
+
+<style>
+#preloader {
+  position: fixed;
+  z-index: 10000;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(14, 14, 14, 0.7);
+  backdrop-filter: blur(100px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#preloader #svg44 {
+  -webkit-animation: rotating 10s infinite linear;
+  animation: rotating 10s infinite linear;
+  margin-right: 20px;
+}
+</style>
